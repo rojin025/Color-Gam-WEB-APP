9b13b5891ce3b5433dc83bff8c5f2da142628c3b
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2019-04-02 23:37:15 -0700

    v.1.1.4
    js

--- colorGame.js
+++ colorGame.js
@@ -1,1 +1,25 @@
-alert ("CONNECTED!!!!!!!!!!");
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(255, 0, 255)"
+]
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++ ){
+  //add initail colors to squuare
+  squares[i].style.background = colors[i];
+
+  //add click listeners to squares
+  squares[i].addEventListener("click",function(){
+    //Grab color of clicked squares
+
+    //Compare color  to pickedColor
+  });
+}
